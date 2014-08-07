93f967f0bb9e49864d1ab518babbdf0bae001125
yuda hermawan <yuda.hermawan28@gmail.com>
2014-08-07 16:04:02 +0700

    make root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create]
+
+  root 'maps#index'
 
   resources :maps
 
-  resources :sessions
+  resources :sessions, except: [:index, :show, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
